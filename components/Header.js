8b008d491e751fd0e53abe58ade169466f59f328
Lucas Arantes <lucas.ruby.dev@gmail.com>
2022-10-02 13:46:24 -0400

    Add menu with my socials using SVG icons

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,16 @@
 import { useState, useEffect } from "react";
 import { useTheme } from "next-themes";
 
+import Link from "next/link";
+import Image from "next/image";
+
+import LightIcon from "../icons/light.svg";
+import DarkIcon from "../icons/dark.svg";
+
+import TwitterIcon from "../icons/twitter.svg";
+import DevToIcon from "../icons/dev-to.svg";
+import GithubIcon from "../icons/github.svg";
+
 function Header() {
   const [mounted, setMounted] = useState(false);
   const { resolvedTheme, setTheme } = useTheme();
@@ -11,31 +21,86 @@ function Header() {
 
   return (
     <div className="flex justify-between my-8 items-center">
-      <h1 className="font-medium text-xl">Next.js Blog</h1>
-      <div>
-        <a
-          href="https://twitter.com/MarkSShenouda"
-          target="_blank"
-          rel="noopener noreferrer"
-          className="mr-4"
-        >
-          Twitter
+      <Link href="/">
+        <a className="hover:bg-gray-100 dark:hover:bg-gray-800 px-4 py-3 rounded">
+          <h1 className="font-medium text-xl">
+            <Image
+              src="/favicon.ico"
+              height={15}
+              width={15}
+              alt="lucasprag emoji"
+            />
+            <span className="ml-2">lucasprag</span>
+          </h1>
         </a>
-        <a
-          href="https://github.com/markshenouda"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Github
-        </a>
+      </Link>
+
+      <div className="flex">
+        <Link href="https://dew.to/lucasprag">
+          <a
+            className="px-4 pb-3 pt-4 ml-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded"
+            target="_blank"
+          >
+            <DevToIcon
+              className="align-middle"
+              height={25}
+              width={25}
+              alt="my dev.to profile"
+            />
+          </a>
+        </Link>
+
+        <Link href="https://github.com/lucasprag">
+          <a
+            className="px-4 pb-3 pt-4 ml-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded"
+            target="_blank"
+          >
+            <GithubIcon
+              className="align-middle"
+              height={25}
+              width={25}
+              alt="my twitter profile"
+            />
+          </a>
+        </Link>
+
+        <Link href="https://twitter.com/lucasprag">
+          <a
+            className="px-4 pb-3 pt-4 ml-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded"
+            target="_blank"
+          >
+            <TwitterIcon
+              className="align-middle"
+              height={25}
+              width={25}
+              alt="my twitter profile"
+            />
+          </a>
+        </Link>
 
         {mounted && (
           <button
+            className="px-4 pb-3 pt-4 ml-1 hover:bg-gray-100 dark:hover:bg-gray-800 rounded"
             onClick={() =>
               setTheme(resolvedTheme === "dark" ? "light" : "dark")
             }
           >
-            toggle ({resolvedTheme})
+            {resolvedTheme === "dark" && (
+              <DarkIcon
+                className="align-middle"
+                height={20}
+                width={20}
+                alt="click to enable dark mode"
+              />
+            )}
+            {resolvedTheme === "light" && (
+              <LightIcon
+                className="align-middle"
+                height={20}
+                width={20}
+                alt="click to enable light mode"
+              />
+            )}
           </button>
         )}
       </div>
