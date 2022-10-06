0e57dd426db7f9e552d66f5856b6ab4ac58709dc
Lucas Arantes <lucas.ruby.dev@gmail.com>
2022-10-06 09:28:57 -0400

    Add more contrast to dates when listing posts in light mode

--- components/PostCard.js
+++ components/PostCard.js
@@ -3,7 +3,7 @@ import Link from "next/link";
 function PostCard({ title, date, slug }) {
   return (
     <div className="flex py-1 px-5 rounded hover:bg-gray-200 dark:hover:bg-gray-800">
-      <time className="w-32 text-center font-medium text-sm py-3 text-gray-400">{date}</time>
+      <time className="w-32 text-center font-medium text-sm py-3 text-gray-700 dark:text-gray-400">{date}</time>
 
       <Link href="/[slug]" as={`/${slug}`}>
         <a className="w-full p-2 ml-5">
