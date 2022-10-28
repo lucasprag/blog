f7eb9f19bff158f32ff575eaa83465f877cb047f
Lucas Arantes <lucas.ruby.dev@gmail.com>
2022-10-28 15:58:28 -0400

    Update intro

--- components/Intro.js
+++ components/Intro.js
@@ -2,11 +2,8 @@ function Intro() {
   return (
     <div className="intro text-md md:text-xl mb-5">
       <div>
-        Software engineer, father, indie hacker from 🇧🇷 living in 🇨🇦
-      </div>
-      <div>
         <span className="inline mb-2 mr-2">Building <a href="https://formigabot.com/" target="_blank" rel="noreferrer">FormigaBot</a>.</span>
-        <span className="inline mb-2 mr-2">Tech Lead at <a href="https://smile.io/careers" target="_blank" rel="noreferrer">Smile.io</a>.</span>
+        <span className="inline mb-2 mr-2">Software Engineer at <a href="https://smile.io/careers" target="_blank" rel="noreferrer">Smile.io</a>.</span>
         <span className="inline mb-2 mr-2">Sharing things I learn here 👇</span>
       </div>
     </div>
