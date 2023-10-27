1e20a56fecd46a89709973596e5ba910deaa6f90
lucasprag <lucasprag@users.noreply.github.com>
2023-10-27 02:54:59 +0000

    deploy: eaad942fbd03856bcd8c1eecd7efd93c6f64193e

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './_drafts/**/*.html',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    './_posts/*.md',
+    './*.md',
+    './*.html',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    theme: {
+      extend: {},
+    },
+  },
+  plugins: []
+}
